4fe6d6cfb1dd394e35a7871d5f3741af8d885a3c
Ayman <ayman.asbai101@alu.ulpgc.es>
2026-03-20 16:45:36 +0000

    refactor: modify the layout of the navigation bar tabs

--- front-end/medsy/app/(tabs)/_layout.tsx
+++ front-end/medsy/app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
+import { NavBar } from '@/components/nav-bar';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -11,10 +12,10 @@ export default function TabLayout() {
 
   return (
     <Tabs
+      tabBar={props => <NavBar {...props} />}
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
@@ -28,6 +29,24 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: 'History',
+        }}
+      />
+      <Tabs.Screen
+        name="family"
+        options={{
+          title: 'Family',
+        }}
+      />
+      <Tabs.Screen
+        name="reminders"
+        options={{
+          href: null,
         }}
       />
     </Tabs>
