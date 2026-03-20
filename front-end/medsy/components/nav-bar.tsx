c8896ac3f2168322113b09d44cf15965b8b0c8f1
Ayman <ayman.asbai101@alu.ulpgc.es>
2026-03-20 16:31:33 +0000

    feat: render tabs

--- front-end/medsy/components/nav-bar.tsx
+++ front-end/medsy/components/nav-bar.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Pressable } from 'react-native';
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 
-export function NavBar() {
+export function NavBar({ state, descriptors, navigation }: BottomTabBarProps) {
   return (
     <View style={styles.container}>
-      <View style={styles.navbar} />
+      <View style={styles.navbar}>
+        <View style={styles.menuWrapper}>
+          {state.routes.map((route, index) => {
+            const isFocused = state.index === index;
+
+            const onPress = () => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route.key,
+                canPreventDefault: true,
+              });
+
+              if (!isFocused && !event.defaultPrevented) {
+                navigation.navigate(route.name);
+              }
+            };
+
+            return (
+              <Pressable key={route.key} style={styles.menuElement} onPress={onPress}>
+                <View
+                  style={[
+                    styles.placeholder,
+                    { opacity: isFocused ? 1 : 0.5 },
+                  ]}
+                />
+              </Pressable>
+            );
+          })}
+        </View>
+      </View>
     </View>
   );
 }
@@ -27,5 +57,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#f1ebebff',
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
+  },
+  menuWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    height: 60,
+  },
+  menuElement: {
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  placeholder: {
+    width: 20,
+    height: 20,
+    backgroundColor: '#000',
+    borderRadius: 10,
   },
 });
